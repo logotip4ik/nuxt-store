37fc22fb0b5273565e02a89ff11b8155ed96d04d
logotip4ik <bogdankostyuk12@gmail.com>
2021-01-16 17:28:04 +0200

    update: added new error layout

--- layouts/error.vue
+++ layouts/error.vue
@@ -1,26 +1,102 @@
 <template>
-  <main class="main">
-    <template v-if="error.statusCode === 404">
-      <h1 class="title mb-2">Page Not Found</h1>
-      <h2 class="subtitle">404</h2>
-      <p class="mb-4">Looks like you've followed a broken link or entered a URL that doesn't exist on this site.</p>
-    </template>
-    <template v-else>
-      <h1 class="title">An error occurred</h1>
-      <p class="mb-4">Looks like you've followed a broken link or entered a URL that doesn't exist on this site.</p>
-    </template>
-    <nuxt-link to="/">
-      <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 16 16" class="inline fill-current">
-        <path d="M11.9998836,4.09370803 L8.55809517,7.43294953 C8.23531459,7.74611298 8.23531459,8.25388736 8.55809517,8.56693769 L12,11.9062921 L9.84187871,14 L4.24208544,8.56693751 C3.91930485,8.25388719 3.91930485,7.74611281 4.24208544,7.43294936 L9.84199531,2 L11.9998836,4.09370803 Z"></path>
+  <div class="__nuxt-error-page">
+    <div class="error">
+      <svg xmlns="http://www.w3.org/2000/svg" width="90" height="90" fill="#DBE1EC" viewBox="0 0 48 48">
+        <path
+          d="M22 30h4v4h-4zm0-16h4v12h-4zm1.99-10C12.94 4 4 12.95 4 24s8.94 20 19.99 20S44 35.05 44 24 35.04 4 23.99 4zM24 40c-8.84 0-16-7.16-16-16S15.16 8 24 8s16 7.16 16 16-7.16 16-16 16z"
+        />
       </svg>
-      Go back
-    </nuxt-link>
-  </main>
+
+      <div class="title">{{ message }}</div>
+      <p v-if="statusCode === 404" class="description">
+        <a v-if="typeof $route === 'undefined'" class="error-link" href="/"><% messages.back_to_home %></a>
+        <NuxtLink v-else class="error-link" to="/"><%= messages.back_to_home %></NuxtLink>
+      </p>
+      <% if(debug) { %>
+      <p class="description" v-else><%= messages.client_error_details %></p>
+      <% } %>
+
+      <div class="logo">
+        <a href="https://nuxtjs.org" target="_blank" rel="noopener"><%= messages.nuxtjs %></a>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'error',
-  props: ['error']
+  name: 'NuxtError',
+  props: {
+    error: {
+      type: Object,
+      default: null
+    }
+  },
+  computed: {
+    statusCode() {
+      return (this.error && this.error.statusCode) || 500
+    },
+    message() {
+      return this.error.message || '<%= messages.client_error %>'
+    }
+  },
+  head() {
+    return {
+      title: this.message,
+      meta: [
+        {
+          name: 'viewport',
+          content: 'width=device-width,initial-scale=1.0,minimum-scale=1.0'
+        }
+      ]
+    }
+  }
 }
 </script>
+
+<style lang="scss">
+.__nuxt-error-page {
+  padding: 1rem;
+  background: #f7f8fb;
+  color: #47494e;
+  text-align: center;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  font-family: sans-serif;
+  font-weight: 100 !important;
+  -ms-text-size-adjust: 100%;
+  -webkit-text-size-adjust: 100%;
+  -webkit-font-smoothing: antialiased;
+  position: absolute;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+
+  .error {
+    max-width: 450px;
+  }
+  .title {
+    font-size: 1.5rem;
+    margin-top: 15px;
+    color: #47494e;
+    margin-bottom: 8px;
+  }
+  .description {
+    color: #7f828b;
+    line-height: 21px;
+    margin-bottom: 10px;
+  }
+  a {
+    color: #7f828b !important;
+    text-decoration: none;
+  }
+  .logo {
+    position: fixed;
+    left: 12px;
+    bottom: 12px;
+  }
+}
+</style>
